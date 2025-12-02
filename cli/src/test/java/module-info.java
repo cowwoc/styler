6caaee83ddcd207d01dfb93e52f9ecd6f6e67afb
Gili Tzabari <cowwoc2020@gmail.com>
2025-12-02 12:02:12 -0500

    config: Add SLF4J provider and incomplete task detection hook
    1. Add logback-classic as test dependency to all modules using
       requirements-java library (which uses SLF4J internally):
       - ast/core, cli, config, parser, security
    
    2. Update test module-info.java files to require ch.qos.logback.classic
    
    3. Create detect-incomplete-tasks.sh SessionStart hook to warn about
       tasks that were merged to main but never cleaned up (missing CLEANUP
       state transition)
    
    Fixes SLF4J "No SLF4J providers were found" warnings during test execution.

--- cli/src/test/java/module-info.java
+++ cli/src/test/java/module-info.java
@@ -6,6 +6,7 @@ module io.github.cowwoc.styler.cli.test
 	requires io.github.cowwoc.styler.cli;
 	requires io.github.cowwoc.requirements12.java;
 	requires org.testng;
+	requires ch.qos.logback.classic;
 
 	opens io.github.cowwoc.styler.cli.test to org.testng;
 }
--- security/src/test/java/module-info.java
+++ security/src/test/java/module-info.java
@@ -3,6 +3,7 @@ module io.github.cowwoc.styler.security.test
 	requires io.github.cowwoc.styler.security;
 	requires org.testng;
 	requires io.github.cowwoc.requirements12.java;
+	requires ch.qos.logback.classic;
 
 	opens io.github.cowwoc.styler.security.test to org.testng;
 }
