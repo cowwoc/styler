350ba14931b0d4bfe680b5d2d2da22eaf4143320
Gili Tzabari <cowwoc2020@gmail.com>
2026-01-15 16:36:03 -0500

    bugfix: parse parameterized array type method references (e.g., Class<?>[]::new)
    - Parser failed on array creation method references with generic types
    - 5 Spring Framework files failing with "Unexpected token: QUESTION_MARK"
    
    - After parsing type arguments in parsePostfix(), check for array dimensions
    - Wrap result in ARRAY_TYPE node when dimensions are present
    
    Task ID: v0.5-fix-wildcard-array-method-reference

--- parser/src/test/java/io/github/cowwoc/styler/parser/test/ParameterizedMethodReferenceParserTest.java
+++ parser/src/test/java/io/github/cowwoc/styler/parser/test/ParameterizedMethodReferenceParserTest.java
@@ -104,6 +104,150 @@ public class ParameterizedMethodReferenceParserTest
 	}
 
 	/**
+	 * Tests constructor reference with wildcard type argument and array dimensions.
+	 * Pattern: {@code Class<?>[]::new}
+	 */
+	@Test
+	public void testWildcardArrayConstructorReference()
+	{
+		String source = """
+			class Test
+			{
+				Object f = Class<?>[]::new;
+			}
+			""";
+		try (Parser parser = parse(source);
+			NodeArena expected = new NodeArena())
+		{
+			NodeArena actual = parser.getArena();
+			expected.allocateNode(NodeType.IDENTIFIER, 25, 30);
+			expected.allocateNode(NodeType.WILDCARD_TYPE, 31, 32);
+			expected.allocateNode(NodeType.ARRAY_TYPE, 25, 35);
+			expected.allocateNode(NodeType.METHOD_REFERENCE, 25, 40);
+			expected.allocateNode(NodeType.FIELD_DECLARATION, 14, 41);
+			expected.allocateClassDeclaration(0, 43, new TypeDeclarationAttribute("Test"));
+			expected.allocateNode(NodeType.COMPILATION_UNIT, 0, 44);
+			requireThat(actual, "actual").isEqualTo(expected);
+		}
+	}
+
+	/**
+	 * Tests constructor reference with upper-bounded wildcard type argument and array dimensions.
+	 * Pattern: {@code List<? extends Number>[]::new}
+	 */
+	@Test
+	public void testUpperBoundedWildcardArrayConstructorReference()
+	{
+		String source = """
+			class Test
+			{
+				Object f = List<? extends Number>[]::new;
+			}
+			""";
+		try (Parser parser = parse(source);
+			NodeArena expected = new NodeArena())
+		{
+			NodeArena actual = parser.getArena();
+			expected.allocateNode(NodeType.IDENTIFIER, 25, 29);
+			expected.allocateNode(NodeType.QUALIFIED_NAME, 40, 46);
+			expected.allocateNode(NodeType.WILDCARD_TYPE, 30, 46);
+			expected.allocateNode(NodeType.ARRAY_TYPE, 25, 49);
+			expected.allocateNode(NodeType.METHOD_REFERENCE, 25, 54);
+			expected.allocateNode(NodeType.FIELD_DECLARATION, 14, 55);
+			expected.allocateClassDeclaration(0, 57, new TypeDeclarationAttribute("Test"));
+			expected.allocateNode(NodeType.COMPILATION_UNIT, 0, 58);
+			requireThat(actual, "actual").isEqualTo(expected);
+		}
+	}
+
+	/**
+	 * Tests constructor reference with generic type argument and array dimensions.
+	 * Pattern: {@code ArrayList<String>[]::new}
+	 */
+	@Test
+	public void testGenericArrayConstructorReference()
+	{
+		String source = """
+			class Test
+			{
+				Object f = ArrayList<String>[]::new;
+			}
+			""";
+		try (Parser parser = parse(source);
+			NodeArena expected = new NodeArena())
+		{
+			NodeArena actual = parser.getArena();
+			expected.allocateNode(NodeType.IDENTIFIER, 25, 34);
+			expected.allocateNode(NodeType.QUALIFIED_NAME, 35, 41);
+			expected.allocateNode(NodeType.QUALIFIED_NAME, 35, 41);
+			expected.allocateNode(NodeType.ARRAY_TYPE, 25, 44);
+			expected.allocateNode(NodeType.METHOD_REFERENCE, 25, 49);
+			expected.allocateNode(NodeType.FIELD_DECLARATION, 14, 50);
+			expected.allocateClassDeclaration(0, 52, new TypeDeclarationAttribute("Test"));
+			expected.allocateNode(NodeType.COMPILATION_UNIT, 0, 53);
+			requireThat(actual, "actual").isEqualTo(expected);
+		}
+	}
+
+	/**
+	 * Tests constructor reference with generic type argument and multi-dimensional array.
+	 * Pattern: {@code List<String>[][]::new}
+	 */
+	@Test
+	public void testGenericMultiDimensionalArrayConstructorReference()
+	{
+		String source = """
+			class Test
+			{
+				Object f = List<String>[][]::new;
+			}
+			""";
+		try (Parser parser = parse(source);
+			NodeArena expected = new NodeArena())
+		{
+			NodeArena actual = parser.getArena();
+			expected.allocateNode(NodeType.IDENTIFIER, 25, 29);
+			expected.allocateNode(NodeType.QUALIFIED_NAME, 30, 36);
+			expected.allocateNode(NodeType.QUALIFIED_NAME, 30, 36);
+			expected.allocateNode(NodeType.ARRAY_TYPE, 25, 41);
+			expected.allocateNode(NodeType.METHOD_REFERENCE, 25, 46);
+			expected.allocateNode(NodeType.FIELD_DECLARATION, 14, 47);
+			expected.allocateClassDeclaration(0, 49, new TypeDeclarationAttribute("Test"));
+			expected.allocateNode(NodeType.COMPILATION_UNIT, 0, 50);
+			requireThat(actual, "actual").isEqualTo(expected);
+		}
+	}
+
+	/**
+	 * Tests method reference on generic array type.
+	 * Pattern: {@code List<String>[]::clone}
+	 */
+	@Test
+	public void testGenericArrayMethodReference()
+	{
+		String source = """
+			class Test
+			{
+				Object f = List<String>[]::clone;
+			}
+			""";
+		try (Parser parser = parse(source);
+			NodeArena expected = new NodeArena())
+		{
+			NodeArena actual = parser.getArena();
+			expected.allocateNode(NodeType.IDENTIFIER, 25, 29);
+			expected.allocateNode(NodeType.QUALIFIED_NAME, 30, 36);
+			expected.allocateNode(NodeType.QUALIFIED_NAME, 30, 36);
+			expected.allocateNode(NodeType.ARRAY_TYPE, 25, 39);
+			expected.allocateNode(NodeType.METHOD_REFERENCE, 25, 46);
+			expected.allocateNode(NodeType.FIELD_DECLARATION, 14, 47);
+			expected.allocateClassDeclaration(0, 49, new TypeDeclarationAttribute("Test"));
+			expected.allocateNode(NodeType.COMPILATION_UNIT, 0, 50);
+			requireThat(actual, "actual").isEqualTo(expected);
+		}
+	}
+
+	/**
 	 * Tests that less-than still works as comparison operator.
 	 * Ensures the fix does not break normal comparison expressions.
 	 */
